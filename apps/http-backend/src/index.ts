2698406ea08c40b1bf3f874f416e376c22feface
JijoThomas7 <jijothomas151@gmail.com>
2025-09-23 08:12:28 -0700

    changees

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -4,7 +4,7 @@ import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 import { CreateuserSchema,SigninSchema,CreateroomSchema } from "@repo/common/types";
 import { prismaClient } from "@repo/db/client";
-
+//add something in the code
 const app = express();
 app.use(express.json());
 
